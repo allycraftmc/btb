5ce5290060e67d7baa858a67dbe91983c11dda2e
Tert0 <tert0byte@gmail.com>
2025-02-22 15:48:14 +0100

    prevent breaking the smithing table

--- src/main/java/de/tert0/btb/listeners/HandleBlockBreak.java
+++ src/main/java/de/tert0/btb/listeners/HandleBlockBreak.java
@@ -1,5 +1,6 @@
 package de.tert0.btb.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -7,7 +8,13 @@ import org.bukkit.event.block.BlockBreakEvent;
 public class HandleBlockBreak implements Listener {
     @EventHandler
     public void onBreakBlock(BlockBreakEvent e) {
-        if(!e.getBlock().getWorld().getName().equals("world")) return;
-        e.setCancelled(true);
+        if(e.getBlock().getWorld().getName().equals("world")) {
+            e.setCancelled(true);
+            return;
+        }
+
+        if(e.getBlock().getType().equals(Material.SMITHING_TABLE)) {
+            e.setCancelled(true);
+        }
     }
 }
