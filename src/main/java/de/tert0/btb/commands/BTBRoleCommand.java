0588eb2363189f5ad3760aca4207c8383f5f8fce
Tert0 <tert0byte@gmail.com>
2025-02-22 15:31:03 +0100

    add target parameter to team and role command for admins

--- src/main/java/de/tert0/btb/commands/BTBRoleCommand.java
+++ src/main/java/de/tert0/btb/commands/BTBRoleCommand.java
@@ -5,6 +5,7 @@ import de.tert0.btb.GameState;
 import de.tert0.btb.Role;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,19 +18,13 @@ import java.util.stream.Collectors;
 public class BTBRoleCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        if(!(commandSender instanceof Player player)) {
+        if(!(commandSender instanceof Player)) {
             commandSender.sendMessage(Component.text("This command can only be executed by a player", NamedTextColor.RED));
             return true;
         }
-        if(BTB.getPlugin().gameState != GameState.Lobby) {
-            if(!BTB.getPlugin().game.isSpectator(player)) {
-                commandSender.sendMessage(Component.text("You can not select a role!", NamedTextColor.RED));
-                return true;
-            }
-        }
 
-        if(args.length != 1) {
-            commandSender.sendMessage(Component.text("usage: /" + label + " <role>"));
+        if(args.length != 1 && args.length != 2) {
+            commandSender.sendMessage(Component.text("usage: /" + label + " <role> [player]"));
             return true;
         }
 
@@ -50,8 +45,34 @@ public class BTBRoleCommand implements CommandExecutor {
             return true;
         }
 
-        BTB.getPlugin().roles.put(player.getUniqueId(), role);
-        BTB.getPlugin().updatePlayerName(player);
+        Player target;
+        if(args.length == 2) {
+            target = Bukkit.getPlayer(args[1]);
+            if(target == null) {
+                commandSender.sendMessage(
+                        Component.text("Could not find player: ", NamedTextColor.RED)
+                                .append(Component.text(args[1], NamedTextColor.GOLD))
+                );
+                return true;
+            }
+
+            if(!target.equals(commandSender) && !commandSender.hasPermission("btb.role.admin")) {
+                commandSender.sendMessage(Component.text("Permission denied", NamedTextColor.RED));
+                return true;
+            }
+        } else {
+            target = (Player) commandSender;
+        }
+
+        if(BTB.getPlugin().gameState != GameState.Lobby) {
+            if(!BTB.getPlugin().game.isSpectator(target)) {
+                commandSender.sendMessage(Component.text("You can not select a role!", NamedTextColor.RED));
+                return true;
+            }
+        }
+
+        BTB.getPlugin().roles.put(target.getUniqueId(), role);
+        BTB.getPlugin().updatePlayerName(target);
         commandSender.sendMessage(Component.text("You have successfully selected ", NamedTextColor.GREEN).append(Component.text(role.name, NamedTextColor.GOLD)));
         return true;
     }
--- src/main/java/de/tert0/btb/commands/BTBTeamCommand.java
+++ src/main/java/de/tert0/btb/commands/BTBTeamCommand.java
@@ -5,6 +5,7 @@ import de.tert0.btb.GameState;
 import de.tert0.btb.Team;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -17,20 +18,14 @@ import java.util.stream.Collectors;
 public class BTBTeamCommand implements CommandExecutor {
     @Override
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
-        if(!(commandSender instanceof Player player)) {
+        if(!(commandSender instanceof Player)) {
             commandSender.sendMessage(Component.text("command must be run by an player!", NamedTextColor.RED));
             return false;
         }
-        if(BTB.getPlugin().gameState != GameState.Lobby) {
-            // Spectators can join a team during the game
-            if(!BTB.getPlugin().game.isSpectator(player) || BTB.getPlugin().gameState != GameState.Playing) {
-                commandSender.sendMessage(Component.text("It is currently not possible to chose a team!", NamedTextColor.RED));
-                return false;
-            }
-        }
 
-        if(args.length != 1) {
-            commandSender.sendMessage(Component.text("usage: /" + label + " <team>"));
+
+        if(args.length != 1 && (args.length != 2)) {
+            commandSender.sendMessage(Component.text("usage: /" + label + " <team> [player]"));
             return false;
         }
 
@@ -51,11 +46,37 @@ public class BTBTeamCommand implements CommandExecutor {
             return false;
         }
 
-        Team currentTeam = Team.getByPlayer(player);
+        Player target;
+        if(args.length == 2) {
+            target = Bukkit.getPlayer(args[1]);
+            if(target == null) {
+                commandSender.sendMessage(
+                        Component.text("Could not find player: ", NamedTextColor.RED)
+                                .append(Component.text(args[1], NamedTextColor.GOLD))
+                );
+                return false;
+            }
+            if(!commandSender.equals(target) && !commandSender.hasPermission("btb.team.admin")) {
+                commandSender.sendMessage(Component.text("Permission denied", NamedTextColor.RED));
+                return false;
+            }
+        } else {
+            target = (Player) commandSender;
+        }
+
+        Team currentTeam = Team.getByPlayer(target);
         if(currentTeam != null) {
             if (currentTeam == team) {
                 commandSender.sendMessage(Component.text("You are already in that team!", NamedTextColor.GREEN));
                 return true;
+            }
+        }
+
+        if(BTB.getPlugin().gameState != GameState.Lobby) {
+            // Spectators can join a team during the game
+            if(!BTB.getPlugin().game.isSpectator(target) || BTB.getPlugin().gameState != GameState.Playing) {
+                commandSender.sendMessage(Component.text("It is currently not possible to chose a team!", NamedTextColor.RED));
+                return false;
             }
         }
 
@@ -64,28 +85,28 @@ public class BTBTeamCommand implements CommandExecutor {
             return false;
         }
 
-        if(BTB.getPlugin().game.isSpectator(player)) {
+        if(BTB.getPlugin().game.isSpectator(target)) {
             if(!BTB.getPlugin().teamStates.get(team)) {
                 commandSender.sendMessage(Component.text("The team is not in the game!", NamedTextColor.RED));
                 return false;
             }
         }
 
-        if(currentTeam != null) BTB.getPlugin().teams.get(currentTeam).remove(player);
-        BTB.getPlugin().teams.get(team).add(player);
-        BTB.getPlugin().updatePlayerName(player);
+        if(currentTeam != null) BTB.getPlugin().teams.get(currentTeam).remove(target);
+        BTB.getPlugin().teams.get(team).add(target);
+        BTB.getPlugin().updatePlayerName(target);
 
         // Note: Player does not need to be removed from the previous team, as Minecraft takes care of that.
         // (Each Player/Entity can be at most in one team)
         org.bukkit.scoreboard.Team nTeam = BTB.getPlugin().scoreboard.getTeam(team.name);
         assert nTeam != null;
-        nTeam.addPlayer(player);
+        nTeam.addPlayer(target);
 
-        if(BTB.getPlugin().game.isSpectator(player)) {
-            BTB.getPlugin().game.removeSpectator(player);
-            BTB.getPlugin().game.resetPlayer(player);
-            BTB.getPlugin().game.setupPlayer(player);
-            player.teleport(team.getSpawnPoint(BTB.getPlugin().btbWorld));
+        if(BTB.getPlugin().game.isSpectator(target)) {
+            BTB.getPlugin().game.removeSpectator(target);
+            BTB.getPlugin().game.resetPlayer(target);
+            BTB.getPlugin().game.setupPlayer(target);
+            target.teleport(team.getSpawnPoint(BTB.getPlugin().btbWorld));
         }
 
         commandSender.sendMessage(Component.text("You joined Team ").append(Component.text(team.name, team.color)));
